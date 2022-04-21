d18fa8c9fd067f4b7681f87b3386f497044534f7
Dan <daniel.liemng@gmail.com>
2022-04-21 17:25:01 -0400

    about

--- frontend_react/src/containers/About/About.js
+++ frontend_react/src/containers/About/About.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './About.scss';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+import { urlFor, client } from '../../client';
+
+// const abouts = [
+//   {
+//     title: 'Web Development',
+//     description: 'I am a good developer',
+//     imgUrl: images.about01,
+//   },
+//   {
+//     title: 'Frontend Development',
+//     description: 'I am a good designer',
+//     imgUrl: images.about02,
+//   },
+//   {
+//     title: 'Backend Development',
+//     description: 'I am a good developer',
+//     imgUrl: images.about03,
+//   },
+//   {
+//     title: 'MERN stack',
+//     description: 'I am a good developer',
+//     imgUrl: images.about04,
+//   },
+// ];
 
 const About = () => {
-  return <div>About</div>;
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "about"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
+  console.log('AAA', abouts);
+
+  return (
+    <div className='app__about'>
+      <h2 className='head-text'>
+        I Know that <span>Good Development</span>
+        <br />
+        means <span>Good Business</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profiles-item'
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default About;
